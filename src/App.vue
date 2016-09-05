9122458ca4eabdc4d547bbc793339bbf53f1080d
sconfield <isconfield@gmail.com>
2016-09-05 17:24:57 +0800

    网站导航栏组件化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="wall blue lighten-3">
+    <app-nav></app-nav>
     <tile-paper v-for="(index, tile) in tileList | filterBy tileName in 'name'"
       v-bind:paper="tile"
       v-bind:idx="index"
@@ -19,13 +20,14 @@
 <script>
 import TilePaper from './components/TilePaper'
 import TileNav from './components/TileNav'
+import AppNav from './components/AppNav'
 import tileData from './data/Tile'
 import {putCurrentPaperCenter} from './tools/PaperTools'
 
 export default {
   data: function(){
     return {
-      tileList: tileData,
+      tileList: tileData.slice(0, 6),
       tileName: ''
     };
   },
@@ -47,12 +49,18 @@ export default {
   },
   components: {
     TilePaper,
-    TileNav
+    TileNav,
+    AppNav
   }
 }
 </script>
 
 <style>
+html, body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
 .wall {
   width: 100%;
   height: 100%;
---
+++ src/components/AppNav.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <nav class="top-nav">
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo center">www.hao2home.com</a>
+      <ul class="left hide-on-med-and-down">
+        <li class="active"><a href="#">首页</a></li>
+        <li><a href="#">AutoCAD</a></li>
+        <li><a href="#">瓷砖墙</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  computed: {},
+  ready() {},
+  attached() {},
+  methods: {},
+  components: {}
+};
+</script>
+
+<style lang="css" scoped>
+.nav-wrapper {
+  z-index: 999;
+  background-color: #27546F;
+}
+.top-nav {
+  background-color: #27546F;
+}
+</style>
